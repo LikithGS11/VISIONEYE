13d245c2380414ff8409d4e2fabd24bd50247570
Likith G S <148687411+LikithGS11@users.noreply.github.com>
2025-04-19 20:58:11 +0530

    Update ocr_processing.py

--- utils/ocr_processing.py
+++ utils/ocr_processing.py
@@ -2,6 +2,7 @@ from PIL import Image
 import pytesseract
 
 # ✅ Set the correct path to tesseract.exe
+#BEFORE RUNNING IT YOU SHOULD DOWNLOAD TESSERACT FROM ONLINE AND PROVIDE THE PATH BELOW 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Likith G S\Desktop\JUPYTER NOTEBOOK\tesseract.exe"
 
 # Function to extract text from an image using Tesseract OCR
